612900a2904f0d2397abf876ae89bbaf3a6ed74c
masahiko <ichigyo.zanmai@gmail.com>
2018-08-19 18:04:53 +0900

    tools.rs add Nu8, Ni64. And create chars_to_string function.

--- src/tools.rs
+++ src/tools.rs
@@ -1,15 +1,42 @@
 use std::str::FromStr;
 
+// Helper function.
+// Convert from Vec<char> to String.
+// e.g.) vec!['1', '2', '3'] => String "123"
+pub fn chars_to_string(chars: &Vec<char>) -> String {
+    let vec_str: Vec<String> = chars.iter()
+        .map(|ref v| v.to_string()).collect();
+    let result = vec_str.concat();
+    result
+}
+
+// Create u8 from chars.
+pub struct Nu8{pub attr: u8}
+
+impl Nu8 {
+    pub fn new(attr: &Vec<char>) -> Nu8 {
+        let n = chars_to_string(&attr);
+        Nu8 {attr: u8::from_str(&n).unwrap()}
+    }
+}
+
 // Create i32 from chars.
 pub struct Ni32{pub attr: i32}
 
 impl Ni32 {
     pub fn new(attr: &Vec<char>) -> Ni32 {
-        let num_chars: Vec<String> = attr.iter()
-            .map(|ref n| n.to_string()).collect();
-        
-        let num = &num_chars.concat();
-        Ni32 {attr: i32::from_str(&num).unwrap()}
+        let n = chars_to_string(&attr);
+        Ni32 {attr: i32::from_str(&n).unwrap()}
+    }
+}
+
+// Create i64 from chars.
+pub struct Ni64{pub attr: i64}
+
+impl Ni64 {
+    pub fn new(attr: &Vec<char>) -> Ni64 {
+        let n = chars_to_string(&attr);
+        Ni64 {attr: i64::from_str(&n).unwrap()}
     }
 }
 
@@ -18,11 +45,6 @@ pub struct Literal{pub attr: String}
 
 impl Literal {
     pub fn new(attr: &Vec<char>) -> Literal {
-        let str_chars: Vec<String> = attr.iter()
-            .map(|ref s| s.to_string()).collect();
-        
-        let lit = &str_chars.concat();
-        
-        Literal {attr: lit.to_string()}
+        Literal {attr: chars_to_string(&attr)}
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,31 @@
 extern crate onigiri;
-use onigiri::tools::{Ni32, Literal};
+use onigiri::tools;
+use onigiri::tools::{Nu8, Ni32, Ni64, Literal};
+
+#[test]
+fn test_chars_to_string() {
+    let test_chars = vec!['*', 'h', 'e', '1', '1', '0', '*'];
+    assert_eq!(tools::chars_to_string(&test_chars), "*he110*".to_string());
+}
 
 #[test]
 fn test_create_new_number_struct() {
-    let test_vec = vec!['-', '2', '1'];
+    let test_pos_vec = vec!['2', '5', '5'];
+    let test_neg_vec = vec!['-', '2', '1'];
 
-    let new_number = Ni32::new(&test_vec);
-    assert_eq!(&new_number.attr, &-21_i32);
+    let new_u8 = Nu8::new(&test_pos_vec);
+    // If 256, error! u8 max_value is 255.
+    assert_eq!(&new_u8.attr, &255_u8);
+    
+    let new_i32 = Ni32::new(&test_neg_vec);
+    assert_eq!(&new_i32.attr, &-21_i32);
+    assert_eq!(&new_i32.attr + 21_i32, 0);
 
-    let new_literal = Literal::new(&test_vec);
+    let new_i64 = Ni64::new(&test_neg_vec);
+    assert_eq!(&new_i64.attr, &-21_i64);
+    assert_eq!(!&new_i64.attr, 20);
+
+    let new_literal = Literal::new(&test_neg_vec);
     assert_eq!(&new_literal.attr, &"-21".to_string());
+    assert_eq!(&new_literal.attr.replace("-", "+"), &"+21".to_string());
 }
