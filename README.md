116704eee433d285dacbd896324efe604ed62e2c
masahiko <ichigyo.zanmai@gmail.com>
2018-08-21 10:57:07 +0900

    Modified tests/lib.rs. Add example to README.md

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ The japanese name of rice ball is "Onigiri".
 In my image, a grain of rice is `char`. 
 And what collected them is `chars`. 
 And the seasoning of it is each structure of `onigiri::tools`.
+
+## Usage
+
+```
+extern crate onigiri;
+use onigiri::tools;
+
+fn main() {
+    let test_text = "-123 456".to_string();
+    let new_vvchar = tools::create_vvchar(&test_text);
+
+    let new_ni32 = tools::Ni32::new(&new_vvchar[0]);
+    let new_ni32_2 = tools::Ni32::new(&new_vvchar[1]);
+
+    let addition = new_ni32.attr + new_ni32_2.attr;
+    assert_eq!(addition, 333_i32);
+}
+```
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,31 +1,15 @@
 extern crate onigiri;
 use onigiri::tools;
-use onigiri::tools::{Nu8, Ni32, Ni64, Literal};
+
 
 #[test]
-fn test_chars_to_string() {
-    let test_chars = vec!['*', 'h', 'e', '1', '1', '0', '*'];
-    assert_eq!(tools::chars_to_string(&test_chars), "*he110*".to_string());
-}
+fn test_a_series_of_usage() {
+    let test_text = "-123 456".to_string();
+    let new_vvchar = tools::create_vvchar(&test_text);
 
-#[test]
-fn test_create_new_number_struct() {
-    let test_pos_vec = vec!['2', '5', '5'];
-    let test_neg_vec = vec!['-', '2', '1'];
+    let new_ni32 = tools::Ni32::new(&new_vvchar[0]);
+    let new_ni32_2 = tools::Ni32::new(&new_vvchar[1]);
 
-    let new_u8 = Nu8::new(&test_pos_vec);
-    // If 256, error! u8 max_value is 255.
-    assert_eq!(&new_u8.attr, &255_u8);
-    
-    let new_i32 = Ni32::new(&test_neg_vec);
-    assert_eq!(&new_i32.attr, &-21_i32);
-    assert_eq!(&new_i32.attr + 21_i32, 0);
-
-    let new_i64 = Ni64::new(&test_neg_vec);
-    assert_eq!(&new_i64.attr, &-21_i64);
-    assert_eq!(!&new_i64.attr, 20);
-
-    let new_literal = Literal::new(&test_neg_vec);
-    assert_eq!(&new_literal.attr, &"-21".to_string());
-    assert_eq!(&new_literal.attr.replace("-", "+"), &"+21".to_string());
+    let addition = new_ni32.attr + new_ni32_2.attr;
+    assert_eq!(addition, 333_i32);
 }
